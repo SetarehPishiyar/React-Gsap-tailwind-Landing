08fb97b3143758650d941b14d155ddfdaaa586c0
SetarehPishiyar <pishiyarsetareh@gmail.com>
2025-10-16 21:34:37 +0330

    Hero section and gsap animations added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import HeroSection from "./sections/HeroSection";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <NavBar></NavBar>
       <HeroSection></HeroSection>
+      <div className="h-dvh border border-red-500"></div>
     </main>
   );
 };
